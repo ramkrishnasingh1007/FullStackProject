816492a6aef3dc8c17f3005a0dd7012799b6ccf0
ramkrishnasingh1007 <ramkrishnasingh1007@gmail.com>
2024-01-24 20:19:06 +0000

    Final-code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Calendar1 from './components/Calendar1';
-import Calendar2 from './components/Calendar2';
-import { useState } from 'react';
+import CombinedCalendar from './components/CombinedCalendar';
+import Footer from './components/Footer.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
-  const [showComponent1, setShowComponent1] = useState(true);
-
-  const handleButtonClick = () => {
-    // Toggle between components
-    setShowComponent1(!showComponent1);
-  };
   return (
-    <div className='bg-cyan-500 h-full w-full'>
-
-<div
-  className="bg-cyan-500 h-full flex justify-center items-center"
->
-  <button
-    onClick={handleButtonClick}
-    className="bg-red-500 text-white rounded-md py-2 px-4 border-2 border-orange-500 cursor-pointer     mt-8"
-    style={{ display: 'block', margin: 'auto' }}
-  >
-    Switch To {showComponent1 ? "Calendar1" : "Calendar2"}
-  </button>
-</div>
-{showComponent1 ? <Calendar1/> : <Calendar2/>}
- 
-</div>
-
+    <div className='bg-gray-100 h-full w-full'>
+      
+      <Navbar/>
+      <CombinedCalendar/>
+    <Footer/>
+    </div>
   );
 }
 
